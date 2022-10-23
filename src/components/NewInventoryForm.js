1022b2f057437ab516c327a609a86030c51cfe2f
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-10-22 18:02:13 -0600

    Update NewInventoryForm.js

--- src/components/NewInventoryForm.js
+++ src/components/NewInventoryForm.js
@@ -1,10 +1,62 @@
 import React from "react";
 
+
 function NewInventoryForm(){
+
+  function handleNewInventoryFormSubmission(event){
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(event.target.origin.value);
+    console.log(event.target.price.value);
+    console.log(event.target.quantity.value);
+    console.log(event.target.roast.value);
+  }
 
   return (
     <React.Fragment>
-      <p> This is my form </p>
+      <form onSubmit={handleNewInventoryFormSubmission}>
+        <input 
+          type="text"
+          name="name"
+          placeholder="Name of Bean"
+        />
+        <br/>
+        <input 
+          type="text"
+          name="origin"
+          placeholder="Origin of Coffee Bean"
+        />
+        <br/>
+        <input 
+          type="number"
+          min="0.00"
+          step="0.01"
+          name="price"
+          placeholder="Prive per Sac"
+        />
+        <br/>
+        <input
+          type="number"
+          name="quantity"
+          min="1"
+          placeholder="Initial Quantity"
+        />
+        <br/>
+        <input
+          type="text"
+          name="roast"
+          placeholder="Roast Type"
+          list="roastType"
+        />
+        <datalist id="roastType">
+            <option value = "Dark" />
+            <option value = "Medium" />
+            <option value = "Light" />
+        </datalist>
+        <br/>
+        <br/>
+        <button type="submit">Add Inventory</button>
+      </form>
     </React.Fragment>
   );
 }
